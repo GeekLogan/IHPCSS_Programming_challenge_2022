bf6de3917f8a54f161d05b87815c5141b80dec56
Logan Walker <logan.walker@me.com>
2022-06-23 15:22:18 +0300

    Update cpu.c

--- src/c/cpu.c
+++ src/c/cpu.c
@@ -162,7 +162,6 @@ int main(int argc, char* argv[])
 		// -- SUBTASK 2: PROPAGATE TEMPERATURES -- //
 		/////////////////////////////////////////////
 		#pragma omp for
-		#pragma omp tile sizes(32,32)
 		for(int i = 1; i <= ROWS_PER_MPI_PROCESS; i++)
 		{
 			// Process the cell at the first column, which has no left neighbour
